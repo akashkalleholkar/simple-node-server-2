7998231fee2a111256edc3c658b8d7aa7fea7ecc
Ankit <43137695+andalike@users.noreply.github.com>
2022-01-12 20:32:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var server = http.createServer(function (req, res) {
     }
     else if (req.url == "/dashboard") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Dashboard Page.</p></body></html>');
+        res.write('<html><body><p>This is the Dashboard Page.</p></body></html>');
         res.end();
     }
     else
