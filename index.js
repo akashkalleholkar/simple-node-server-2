ccb488636a94e8a3301417a5107a47c3cf171311
Ankit <43137695+andalike@users.noreply.github.com>
2022-01-12 17:38:28 +0530

    Init Files Added.

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    if (req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Default Page.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/home") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Home Page.</p></body></html>');
+        res.end();
+
+    }
+    else if (req.url == "/dashboard") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Dashboard Page.</p></body></html>');
+        res.end();
+    }
+    else
+        res.end('Invalid Request!');
+});
+
+server.listen(5000);
+console.log('Node.js web server at port 5000 is running..')
---
+++ package.json
@@ -0,0 +1,13 @@
+{
+  "name": "nodeapp",
+  "version": "0.0.1",
+  "description": "NodeJS Server on Port 5000",
+  "author": "Ankit SAxena <ankit.saxena@rocketmail.com>",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.16.1"
+  }
+}
