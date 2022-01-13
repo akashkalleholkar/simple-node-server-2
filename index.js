e3b48716e91d14e8667e59743b4f95903ae3fed6
Ankit <43137695+andalike@users.noreply.github.com>
2022-01-13 08:33:46 +0530

    CR 1091 : Default Message Change
    CR 1091 : Default Message Change for default Home Page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Default Page.</p></body></html>');
+        res.write('<html><body><p>This is the Default Page. If You dont specify any address This page will be shown.</p></body></html>');
         res.end();
 
     }
